ca19041e546e321b75dea8a9f7cbeca8958180ad
vm2124 <vm21242124@gmail.com>
2023-04-01 00:38:57 +0530

    codepallete

--- src/components/pallate/CodePallete.jsx
+++ src/components/pallate/CodePallete.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import "./CodePallete.css";
 import NumberedTextarea from "../codeplate/NumberedTextArea";
+import {VscRunAbove,VscClose} from 'react-icons/vsc'
+import {BiGitCompare} from 'react-icons/bi'
+import {SlOptions} from 'react-icons/sl'
 const CodePallete = () => {
-  const [fileOpen, setFileopen] = useState(true);
+  const [fileOpen, setFileopen] = useState(false);
   return (
-    <div className="codepallet">
-      <div className="topmenufile"></div>
-      {fileOpen ? (
-        <div className="infor">
-          <p>welcome to vmstudio</p>
+    <>
+      <div className="topmenufile">
+        <div className="openfiles"><span>vinod.java <VscClose/></span></div>
+        <div className="rightrunoptions">
+          <span><VscRunAbove/></span>
+          <span><BiGitCompare/></span>
+          <span><SlOptions/></span>
         </div>
-      ) : (
-        <div className="plate">
-          <NumberedTextarea />
-        </div>
-      )}
-    </div>
+      </div>
+      <div className="codepallet">
+        {fileOpen ? (
+          <div className="infor">
+            <p>welcome to vmstudio</p>
+          </div>
+        ) : (
+          <div className="plate">
+            <NumberedTextarea />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
